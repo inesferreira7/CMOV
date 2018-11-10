9e648c75f8e279ae69b33f4a64afde3d90930e46
Nuno Miguel <up201406990@fe.up.pt>
2018-11-10 15:48:12 +0000

    Added status codes to customer responses

--- API/api/controllers/customerController.js
+++ API/api/controllers/customerController.js
@@ -3,15 +3,23 @@
 var mongoose = require('mongoose'),
     Customer = require('../models/customerModel');
 
-// Lists all app customers
+/*
+ * Retrieves all customers.
+ * If an error is encountered, sends a response with a 500 status code and the error description.
+ * If no error is encountered, sends a response with a 200 status code.
+ */
 exports.getAllCustomers = (req, res) => {
-  Customer.find({}, (err, customer) => {
-    if (err) res.send(err);
-    res.json(customer);
+  Customer.find({}, (err) => {
+    if (err) res.status(500).send(err);
+    res.status(200).send();
   });
 };
 
-// Creates a new app customer
+/*
+ * Creates a new app customer.
+ * If an error is encountered, sends a response with a 500 status code and the error description.
+ * If no error is encountered, sends a response with a 200 status code and the UUID of the customer. 
+ */
 exports.registerCustomer = (req, res) => {
     var customer = new Customer({
         name: req.body.name,
@@ -19,35 +27,43 @@ exports.registerCustomer = (req, res) => {
         credit_card: req.body.credit_card
     });
 
-    customer.save()
-    .then(data => {
-        res.send(data);
-    })
-    .catch(err => {
-        res.send(err);
+    // On save, sends the UUID of the customer back to the app.
+    customer.save((err, data) => {
+        if (err) res.status(500).send(err);
+        res.status(200).send(data._id);
     });
 };
 
-// Retrieves a customer with a certain ID
+/* Retrieves a customer with a certain ID.
+ * If an error is encountered, sends a response with a 500 status code and the error description.
+ * If no error is encountered, sends a response with a 200 status code and the found customer.
+ */
 exports.getCustomer = (req, res) => {
     Customer.findById(req.params.customerId, (err, customer) => {
-        if (err) res.send(err);
-        res.json(customer);
+        if (err) res.status(500).send(err);
+        res.status(200).send(customer);
     });
 };
 
-// Updates a customer with a certain ID
+/* Updates a customer with a certain ID.
+ * If an error is encountered, sends a response with a 500 status code and the error description.
+ * If no error is encountered, sends a response with a 200 status code.
+ */
 exports.updateCustomer = (req, res) => {
-    Customer.findOneAndUpdate({_id: req.params.customerId}, req.body, {new: true}, (err , customer) => {
-        if (err) res.send(err);
-        res.json(customer);
+    console.log(req.body);
+    Customer.findOneAndUpdate({_id: req.params.customerId}, req.body, {returnNewDocument: false}, (err) => {
+        if (err) res.status(500).send(err);
+        res.status(200).send();
     });
 }
 
-// Deletes a customer with a certain ID
+/* Deletes a customer with a certain 
+ * If an error is encountered, sends a response with a 500 status code and the error description.
+ * If no error is encountered, sends a response with a 200 status code.
+ */
 exports.deleteCustomer = (req, res) => {
-    Customer.remove({ _id: req.params.customerId }, (err, customer) => {
-        if (err) res.send(err);
-        res.json({message: 'Customer successfully deleted'});
+    Customer.remove({ _id: req.params.customerId }, (err) => {
+        if (err) res.status(500).send(err);
+        res.status(200).send();
     })
 }
