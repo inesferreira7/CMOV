15c9bf3b7024860379ceffb0fe5e13e4cfa85e5c
Unknown <inesferreira789@gmail.com>
2018-11-11 17:34:09 +0000

    Deleted unwanted element

--- CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/ShowsFragment.java
+++ CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/ShowsFragment.java
@@ -6,18 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ShowsFragment extends Fragment {
-
 
     public ShowsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
