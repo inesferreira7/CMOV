e1db138e68da890d381cf1d3afa46ffcad9b9c8c
Nuno Miguel <up201406990@fe.up.pt>
2018-11-13 13:45:23 +0000

    Scanner prototype working

--- TERMINAL/app/src/main/java/com/example/nuno/terminal/MainActivity.java
+++ TERMINAL/app/src/main/java/com/example/nuno/terminal/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.nuno.terminal;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button scanButton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanButton = (Button) findViewById(R.id.scanButton);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scan(true);
+            }
+        });
+    }
+
+    public void scan(boolean qrcode) {
+        try {
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", qrcode ? "QR_CODE_MODE" : "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        }
+        catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+
+                //TODO read as json
+                /*
+                1- a field will be type of message (ticket or cafeteria)
+                2- another field will be the data
+                {
+                    type: ...,
+                    data: {
+                        ...
+                    }
+                }
+                 */
+                Log.i("Data", contents);
+            }
+        }
     }
 }
