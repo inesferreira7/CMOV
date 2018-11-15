234d77fdc48db249d3dfc7ab40178f213e50d46e
Nuno Miguel <up201406990@fe.up.pt>
2018-11-15 14:24:20 +0000

    Ticket information in fragment. Needs display

--- CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/MyTicketsFragment.java
+++ CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/MyTicketsFragment.java
@@ -1,12 +1,34 @@
 package musicline.cmov.org.feup.musicline;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class MyTicketsFragment extends Fragment {
+
+    SharedPreferences prefs;
+    JSONArray tickets_json;
 
     public MyTicketsFragment() {
         // Required empty public constructor
@@ -15,8 +37,33 @@ public class MyTicketsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shows, container, false);
-        
+
+        prefs = view.getContext().getSharedPreferences(Globals.PREFERENCES_NAME, MODE_PRIVATE);
+        tickets_json = new JSONArray();
+
+        listTickets(tickets_json);
+
         return view;
+    }
+
+    public void listTickets(final JSONArray ticketList) {
+        RequestQueue queue = Volley.newRequestQueue(this.getContext());
+        String url = Globals.URL + "/tickets/customer/" + prefs.getString("uuid", "");
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, ticketList, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i("Response", response.toString());
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error API call", error.toString());
+                    }
+                });
+
+        queue.add(request);
     }
 
 }
