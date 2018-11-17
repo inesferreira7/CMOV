413b45b4e8dded8455f149ea40856a127e4289c7
Unknown <inesferreira789@gmail.com>
2018-11-17 13:40:20 +0000

    Implemented buy tickets, needs request

--- CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/Globals.java
+++ CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/Globals.java
@@ -1,11 +1,14 @@
 package musicline.cmov.org.feup.musicline;
 
+import java.text.DecimalFormat;
+
 public class Globals {
     public static String PREFERENCES_NAME = "UserPreferences";
-    public static String URL = "https://57ec3000.ngrok.io";
+    public static String URL = "https://d9e790d5.ngrok.io";
     public enum Item {Coffee, Soda, Popcorn, Sandwich};
     public static double COFFEE_PRICE = 1.00;
     public static double SODA_PRICE = 1.50;
     public static double POPCORN_PRICE = 2.00;
     public static double SANDWICH_PRICE = 3.00;
+    public static DecimalFormat df = new DecimalFormat("#.00");
 }
--- CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/ShowActivity.java
+++ CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/ShowActivity.java
@@ -4,10 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+
+import org.w3c.dom.Text;
 
 import musicline.cmov.org.feup.musicline.utils.Show;
 
 public class ShowActivity extends AppCompatActivity {
+
+    TextView title, date, price;
+    ElegantNumberButton quantity_tickets;
+    String actual_quantity;
+    Button buy_tickets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,33 @@ public class ShowActivity extends AppCompatActivity {
 
         Intent i = getIntent();
         Show show = (Show)i.getSerializableExtra("Show");
+
+        title = (TextView)findViewById(R.id.showpage_title);
+        title.setText(show.getName());
+
+        date = (TextView)findViewById(R.id.date_info);
+        date.setText("   " + show.getDate());
+
+        price = (TextView)findViewById(R.id.price_info);
+        price.setText("   " + show.getTicketPrice().toString() + "€");
+
+        quantity_tickets = (ElegantNumberButton)findViewById(R.id.ticket_button);
+        quantity_tickets.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actual_quantity = quantity_tickets.getNumber();
+            }
+        });
+
+        buy_tickets = (Button)findViewById(R.id.buyTickets_button);
+        buy_tickets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("quantidade" , actual_quantity);
+            }
+        });
+
+
+
     }
 }
