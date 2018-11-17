ab80d45ddf6babe7fb13f55f2ab560862ffa8efa
Nuno Miguel <up201406990@fe.up.pt>
2018-11-17 21:17:16 +0000

    Update ShowActivity.java

--- CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/activities/ShowActivity.java
+++ CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/activities/ShowActivity.java
@@ -1,6 +1,10 @@
-package musicline.cmov.org.feup.musicline.activities;
+package musicline.cmov.org.feup.musicline;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,17 +12,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 
-import musicline.cmov.org.feup.musicline.R;
-import musicline.cmov.org.feup.musicline.objects.Show;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import musicline.cmov.org.feup.musicline.utils.Show;
+import musicline.cmov.org.feup.musicline.utils.Ticket;
 
 public class ShowActivity extends AppCompatActivity {
 
-    TextView title, date, price;
+    TextView title, date, price, total_price;
     ElegantNumberButton quantity_tickets;
     String actual_quantity;
     Button buy_tickets;
+    final List<Ticket> ticket_list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +47,9 @@ public class ShowActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show);
 
         Intent i = getIntent();
-        Show show = (Show)i.getSerializableExtra("Show");
+        final Show show = (Show)i.getSerializableExtra("Show");
+
+        actual_quantity = "1";
 
         title = (TextView)findViewById(R.id.showpage_title);
         title.setText(show.getName());
@@ -37,11 +60,17 @@ public class ShowActivity extends AppCompatActivity {
         price = (TextView)findViewById(R.id.price_info);
         price.setText("   " + show.getTicketPrice().toString() + "€");
 
+        total_price = (TextView)findViewById(R.id.total_price_info);
+        total_price.setText("   " + show.getTicketPrice().toString() + "€");
+        // total_price = quantity * price
+
+
         quantity_tickets = (ElegantNumberButton)findViewById(R.id.ticket_button);
         quantity_tickets.setOnClickListener(new ElegantNumberButton.OnClickListener() {
             @Override
             public void onClick(View view) {
                 actual_quantity = quantity_tickets.getNumber();
+                total_price.setText("   " + Integer.toString(Integer.parseInt(actual_quantity) * show.getTicketPrice().intValue()) + "€");
             }
         });
 
@@ -50,10 +79,75 @@ public class ShowActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.e("quantidade" , actual_quantity);
+                buyTickets(show);
             }
         });
+    }
 
+    public void buyTickets(Show show) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = Globals.URL + "/tickets/buy";
+        SharedPreferences prefs = this.getSharedPreferences(Globals.PREFERENCES_NAME, MODE_PRIVATE);
 
+        JSONObject body = new JSONObject();
+        Random rand = new Random();
 
+        int  seat = rand.nextInt(500) + 1;
+
+        try {
+            body.put("performanceName", show.getName());
+            body.put("performanceDate", show.getDate());
+            body.put("performanceId", show.getId());
+            body.put("customerId", prefs.getString("uuid", ""));
+            body.put("seat", seat);
+            body.put("quantity", actual_quantity);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray array = new JSONArray();
+
+        array.put(body);
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, url, array,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray tickets) {
+                        Log.i("Ticket response", tickets.toString());
+
+                        for(int i = 0; i < tickets.length(); i++){
+                            try {
+                                JSONObject t = (JSONObject) tickets.get(i);
+
+                                Ticket ticket = new Ticket(
+                                        t.getString("performanceName"),
+                                        t.getString("performanceDate"),
+                                        t.getString("performanceId"),
+                                        t.getString("customerId"),
+                                        t.getString("seat"),
+                                        t.getBoolean("isUsed")
+                                );
+
+                                ticket_list.add(ticket);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        SharedPreferences.Editor editor = getSharedPreferences(Globals.PREFERENCES_NAME, MODE_PRIVATE).edit();
+                        //TODO por a lista nas shared preferences
+                        editor.apply();
+
+                        //TODO Voltar para a lista de shows
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley error", error.toString());
+                    }
+                });
+
+        queue.add(request);
     }
 }
