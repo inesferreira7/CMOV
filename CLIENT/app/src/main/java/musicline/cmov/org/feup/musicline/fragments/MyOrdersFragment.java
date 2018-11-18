7204b9fc447b51d3f8e08c2a61b185e5fb68f041
Unknown <inesferreira789@gmail.com>
2018-11-18 23:25:56 +0000

    Implemented order list

--- CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/fragments/MyOrdersFragment.java
+++ CLIENT/app/src/main/java/musicline/cmov/org/feup/musicline/fragments/MyOrdersFragment.java
@@ -1,8 +1,6 @@
 package musicline.cmov.org.feup.musicline.fragments;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,7 +9,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,31 +21,34 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import musicline.cmov.org.feup.musicline.R;
 import musicline.cmov.org.feup.musicline.objects.Order;
-import musicline.cmov.org.feup.musicline.objects.Ticket;
 import musicline.cmov.org.feup.musicline.utils.Globals;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class MyOrdersFragment extends Fragment {
-
-    ArrayList<Order> orders;
-    //OrderAdapter<Order> order_adapter;
+    List<Order> orders = new ArrayList<>();
+    ListView list_all_orders;
+    ArrayAdapter<Order> all_orders_adapter;
 
     SharedPreferences prefs;
     JSONArray orders_json;
     private ProgressBar spinner;
+
+    LinearLayout linearLayout;
 
     public MyOrdersFragment() {
         // Required empty public constructor
@@ -62,6 +67,8 @@ public class MyOrdersFragment extends Fragment {
         orders = new ArrayList<>();
 
         listOrders(orders_json);
+        list_all_orders = (ListView)view.findViewById(R.id.list_all_orders);
+        all_orders_adapter = new OrderAdapter();
 
         return view;
     }
@@ -110,6 +117,8 @@ public class MyOrdersFragment extends Fragment {
                                 e.printStackTrace();
                             }
                         }
+
+                        onSuccess(orders);
                     }
                 },
                 new Response.ErrorListener() {
@@ -123,4 +132,43 @@ public class MyOrdersFragment extends Fragment {
         queue.add(request);
     }
 
+    private void onSuccess(List<Order> orders) {
+        list_all_orders.setAdapter(all_orders_adapter);
+    }
+
+    private class OrderAdapter extends ArrayAdapter<Order> {
+
+        OrderAdapter() {
+            super(MyOrdersFragment.this.getContext(), R.layout.all_orders_adapter, orders);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View row = convertView;
+            if (row == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                row = inflater.inflate(R.layout.all_orders_adapter, parent, false);
+            }
+
+            Order o = orders.get(position);
+            HashMap<String, Integer> products = o.getProducts();
+            Log.e("products", products.toString());
+            linearLayout = ((LinearLayout) row.findViewById(R.id.all_products));
+
+            for (Map.Entry<String, Integer> entry : products.entrySet()) {
+                LinearLayout layout = new LinearLayout(getContext());
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+                TextView product = new TextView(getContext());
+                product.setText(entry.getKey());
+                layout.addView(product);
+                TextView quantity = new TextView(getContext());
+                quantity.setText("  " + Integer.toString(entry.getValue()) +"x");
+                layout.addView(quantity);
+                linearLayout.addView(layout);
+            }
+
+            return row;
+        }
+    }
 }
